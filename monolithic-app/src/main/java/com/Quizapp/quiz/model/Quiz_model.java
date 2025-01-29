5752efadbaebafcf8887610c5bbd8ce881333d7b
rakesh baleri <rrbaleri@gmail.com>
2025-01-29 09:27:31 +0530

    file wrap

---
+++ monolithic-app/src/main/java/com/Quizapp/quiz/model/Quiz_model.java
@@ -0,0 +1,45 @@
+package com.Quizapp.quiz.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Quiz_model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String quiztitle;
+
+    @ManyToMany
+    private List<Question_model> questions;
+    public List<Question_model> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question_model> questions) {
+        this.questions = questions;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuiztitle() {
+        return quiztitle;
+    }
+
+    public void setQuiztitle(String quiztitle) {
+        this.quiztitle = quiztitle;
+    }
+
+
+}
