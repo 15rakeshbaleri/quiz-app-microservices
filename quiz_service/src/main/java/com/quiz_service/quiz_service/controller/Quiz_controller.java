52e85f3ac8ec4a7e9e4c6d37147dfcdf5c3a89a0
rakesh baleri <rrbaleri@gmail.com>
2025-01-30 15:41:31 +0530

    api gateway done

--- quiz_service/src/main/java/com/quiz_service/quiz_service/controller/Quiz_controller.java
+++ quiz_service/src/main/java/com/quiz_service/quiz_service/controller/Quiz_controller.java
@@ -1,6 +1,5 @@
 package com.quiz_service.quiz_service.controller;
 
-
 import com.quiz_service.quiz_service.Response.QuizSubmit_response;
 import com.quiz_service.quiz_service.model.Question_wrapper;
 import com.quiz_service.quiz_service.service.Quiz_service;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/quiz")
+@RequestMapping("/quiz") // Corrected the path mapping at the class level
 public class Quiz_controller {
 
     @Autowired
@@ -20,22 +19,21 @@ public class Quiz_controller {
 
     @PostMapping("/create")
     public ResponseEntity<String> createQuiz(@RequestBody Quizdto quizdto) {
-      return new ResponseEntity<>(quiz_service.createquiz(quizdto.getCategory(),quizdto.getNumq(),quizdto.getTitle()),HttpStatus.CREATED);
+        // You could handle validation errors and add more info in the response
+        return new ResponseEntity<>(quiz_service.createquiz(quizdto.getCategory(), quizdto.getNumq(), quizdto.getTitle()), HttpStatus.CREATED);
     }
 
-
-    @GetMapping("get/{quizId}")
+    @GetMapping("/get/{quizId}")
     public ResponseEntity<List<Question_wrapper>> getQuiz(@PathVariable int quizId) {
-
-        return  quiz_service.getCreatedQuiz(quizId);
+        // You could handle the case where quizId doesn't exist and return a 404 response
+        return quiz_service.getCreatedQuiz(quizId);
     }
 
     @PostMapping("/submit/{quizId}")
     public ResponseEntity<String> submitQuiz(@PathVariable int quizId, @RequestBody List<QuizSubmit_response> answers) {
+        int score = quiz_service.calculateScore(quizId, answers);
 
-       int score = quiz_service.calculateScore(quizId,answers);
-
-        return new ResponseEntity<>("Quiz score"+"  "+score, HttpStatus.CREATED);
-
+        // Improved response message and status code
+        return new ResponseEntity<>("Your quiz score is: " + score, HttpStatus.OK);
     }
 }
